3f14527a1c7e5bdcf671e2f6f2eae36b1634850e
pwolper <philip.wolper@gmail.com>
2023-02-16 18:56:40 +0100

    sim/obs plot and model statistics

--- code/calibration-plot.R
+++ code/calibration-plot.R
@@ -2,6 +2,7 @@
 library(ggplot2)
 library(dplyr)
 library(here)
+library(Metrics)
 
 here::here()
 
@@ -9,7 +10,7 @@ BRS264 <- read.csv("./results/2023-02-15_experiments_all.csv")
 
 
 BRS264$location <- c()
-BRS264$irri <- c()
+BRS264$irrigation <- c()
 BRS264$loc_year <- c()
 BRS264$fungal <- ifelse(BRS264$Label=="SAOG_22_nonirrigated"|
                         BRS264$Label=="SAOG_21_nonirrigated"|
@@ -17,7 +18,7 @@ BRS264$fungal <- ifelse(BRS264$Label=="SAOG_22_nonirrigated"|
 
 for(i in 1:nrow(BRS264)){
   BRS264$location[i] <- unlist(unlist(strsplit(BRS264$Label[i], split = "_"))[1])
-  BRS264$irri[i] <- unlist(unlist(strsplit(BRS264$Label[i], split = "_"))[3])
+  BRS264$irrigation[i] <- unlist(unlist(strsplit(BRS264$Label[i], split = "_"))[3])
   BRS264$loc_year[i] <- gsub("^([^_]+_[^_]+).*","\\1",BRS264$Label[i])
 }
 
@@ -25,20 +26,38 @@ BRS264$irri <- as.factor(BRS264$irri)
 BRS264$location <- as.factor(BRS264$location)
 
 str(BRS264)
-BRS264[BRS264$fungal == "none",]
+BRS264_nof <- BRS264[BRS264$fungal == "none",]
 
 # Plotting of results
 ggplot(BRS264,aes(x=Sim_Yield,y=Obs_Yield))+
-  geom_point(aes(color=location,shape=irri), size = 5)+
+  geom_point(aes(color=location,shape=irrigation), size = 5)+
   geom_abline(slope = 1, intercept = 0) +
   scale_x_continuous(limits = c(1000,5500)) +
   scale_y_continuous(limits = c(1000,5500)) +
-  labs(title = "Simulation of wheat growth in Mato Grosso, Brazil", subtitle = "13 experiments were simulated and compared to the observed yield")
+  # labs(title = "Simulation of wheat growth in Mato Grosso, Brazil", subtitle = "13 experiments were simulated and compared to the observed yield") +
+  theme_light()
 
 # Summary statistics
 
+## r.squared
 lm <- lm(data = BRS264,Sim_Yield ~ Obs_Yield)
 summary(lm)
 
-lm_nof <- lm(data = BRS264[BRS264$fungal == "none",], Sim_Yield ~ Obs_Yield)
+lm_nof <- lm(data = BRS264_nof, Sim_Yield ~ Obs_Yield)
 summary(lm_nof)
+
+##Mean absolute error
+mae(BRS264$Obs_Yield,BRS264$Sim_Yield)
+
+mae(BRS264_nof$Obs_Yield,BRS264_nof$Sim_Yield)
+
+## Root mean squared error
+rmse(BRS264$Obs_Yield,BRS264$Sim_Yield)
+
+rmse(BRS264_nof$Obs_Yield,BRS264_nof$Sim_Yield)
+
+## Index of Agreement (d)
+md(BRS264$Sim_Yield,BRS264$Obs_Yield)
+
+md(BRS264_nof$Sim_Yield,BRS264_nof$Obs_Yield)
+#### Why is md smaller for the nof data??
