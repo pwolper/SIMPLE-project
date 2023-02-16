83bdef0416e7a3d68d3ec36429ce295c695bf75f
pwolper <philip.wolper@gmail.com>
2023-02-16 09:41:31 +0100

    calibration plot

--- code/calibration-plot.R
+++ code/calibration-plot.R
@@ -7,10 +7,23 @@ here::here()
 
 BRS264 <- read.csv("./results/2023-02-15_experiments_all.csv")
 
+
+BRS264$location <- c()
+BRS264$irri <- c()
+BRS264$loc_year <- c()
+
+for(i in 1:nrow(BRS264)){
+  BRS264$location[i] <- unlist(unlist(strsplit(BRS264$Label[i], split = "_"))[1])
+  BRS264$irri[i] <- unlist(unlist(strsplit(BRS264$Label[i], split = "_"))[3])
+  BRS264$loc_year[i] <- gsub("^([^_]+_[^_]+).*","\\1",BRS264$Label[i])
+}
+
+BRS264$irri <- as.factor(BRS264$irri)
+BRS264$location <- as.factor(BRS264$location)
 str(BRS264)
 
 ggplot(BRS264,aes(x=Sim_Yield,y=Obs_Yield))+
-  geom_point()+
+  geom_point(aes(color=location,shape=irri), size = 5)+
   geom_abline(slope = 1, intercept = 0) +
   scale_x_continuous(limits = c(1000,5500)) +
   scale_y_continuous(limits = c(1000,5500))
